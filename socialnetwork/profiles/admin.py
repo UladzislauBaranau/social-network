7887b78e944b408ff0b51484af0fd6b6e4da4724
Uladzislau Baranau <uladzislaubaranau1996@gmail.com>
2021-05-03 17:34:56 +0300

    Customized the admin panel

--- socialnetwork/profiles/admin.py
+++ socialnetwork/profiles/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import ProfileCreationForm
 from .models import Profile
 
+
 # Register your models here.
-admin.site.register(Profile)
+class CustomUserAdmin(UserAdmin):
+    model = Profile
+    add_form = ProfileCreationForm
+
+    fieldsets = (
+        ('Username', {'fields': ('username',)}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'birth_date', 'gender')}),
+        ('Location', {'fields': ('country', 'city')}),
+        ('Contact Info', {'fields': ('email', 'phone')}),
+        ('', {'fields': ('is_active', 'is_staff', 'is_superuser', 'date_joined')}),
+    )
+
+
+admin.site.register(Profile, CustomUserAdmin)
