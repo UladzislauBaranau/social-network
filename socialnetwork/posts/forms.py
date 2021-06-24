f4840c3dfcb846413953a37ae011b7cd7c576280
Vladislav Baranov <uladzislaubaranau1996@gmail.com>
2021-06-24 11:15:18 +0300

    Added the ability to create posts by the user
    Created a new section 'posts'. In the 'profiles/templates' has been
    added a link to 'posts'.

---
+++ socialnetwork/posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField()
+
+    class Meta:
+        model = Post
+        fields = ['content']
--- socialnetwork/posts/views.py
+++ socialnetwork/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import PostForm
+from .models import Post
+
+
+def create_posts_view(request):
+    qs_posts = Post.objects.all().filter(post_author=request.user)
+    form = PostForm()
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.post_author = request.user
+            new_post.save()
+
+    context = {
+        'form': form,
+        'all_posts': qs_posts,
+    }
+
+    return render(request, 'posts/posts.html', context)
